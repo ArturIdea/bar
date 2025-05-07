f6814682175d133fe85bb2a3df69a46d4fda8181
gkarountzos <george3-2001@hotmail.com>
2025-05-07 12:18:15 +0300

    BARAKA-496 updated liveness pills

--- src/ui/components/Dashboard/Statistics/PillsSkeleton.tsx
+++ src/ui/components/Dashboard/Statistics/PillsSkeleton.tsx
@@ -1,14 +1,15 @@
 export default function PillsSkeleton() {
   return (
     <div className="px-6">
-      <div className="bg-gray-100 p-3 rounded-full flex gap-4">
-        {[...Array(3)].map((_, i) => (
-          <div
-            key={i}
-            className="flex items-center space-x-2 px-4 py-2 rounded-full bg-white animate-pulse"
-          >
-            <div className="w-4 h-4 bg-gray-200 rounded-full" />
-            <div className="h-4 w-20 bg-gray-200 rounded-md" />
+      <div className="bg-[#ADD9F4] p-2 rounded-full flex gap-4">
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className="group relative inline-block">
+            <div className="flex items-center space-x-2 px-4 py-3 rounded-full bg-white min-w-[150px] animate-pulse">
+              {/* dot */}
+              <div className="w-4 h-4 bg-gray-200 rounded-full" />
+              {/* text */}
+              <div className="h-4 w-20 bg-gray-200 rounded-md" />
+            </div>
           </div>
         ))}
       </div>
