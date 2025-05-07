8e84a2639a1061d08be76ab5013874953bdfceff
Mahmoud Gawish <m.salah@oxinus.io>
2025-05-07 14:30:21 +0400

    pipline issue

--- src/app/[locale]/dashboard/Charts/page.tsx
+++ src/app/[locale]/dashboard/Charts/page.tsx
@@ -12,7 +12,7 @@ export default function Charts() {
         <NewAccountsAreaChart />
         {/* <CardTypesPieChart /> */}
       </div>
-      <div className="flex items-center justify-between gap-2">
+      <div className="flex items-center gap-2">
         <OnboardingChannelPieChart />
         <AppUsageAgeDistributionRadialChart />
       </div>
