03b793554d5efcb8dd853134558cfccb06425111
George Karountzos <george3-2001@hotmail.com>
2025-05-14 13:11:27 +0300

    BARAKA 496 - Updated skeleton for pills

--- src/ui/components/Dashboard/Statistics/PillsSkeleton.tsx
+++ src/ui/components/Dashboard/Statistics/PillsSkeleton.tsx
@@ -2,9 +2,9 @@ export default function PillsSkeleton() {
   return (
     <div className="px-6">
       <div className="bg-[#ADD9F4] p-2 rounded-full flex gap-4">
-        {[...Array(4)].map((_, i) => (
+        {[...Array(5)].map((_, i) => (
           <div key={i} className="group relative inline-block">
-            <div className="flex items-center space-x-2 px-4 py-3 rounded-full bg-white min-w-[150px] animate-pulse">
+            <div className="flex items-center space-x-2 px-4 h-9 rounded-full bg-white min-w-[170px] animate-pulse">
               {/* dot */}
               <div className="w-4 h-4 bg-gray-200 rounded-full" />
               {/* text */}
