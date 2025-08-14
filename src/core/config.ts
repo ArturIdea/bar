31e1ec37d512045d7fa4c361bd1d39a04344192d
ArturZzZZzZ <artur.zub@ideasoft.io>
2025-08-14 14:11:16 +0300

    env

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'https://baraka-app-api-development.uz-pay-dev.ox.one';
 export const KEYCLOAK_URL = process.env.NEXT_PUBLIC_KEYCLOAK_URL || '';
 export const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI || '';
 export const USER_TYPE = process.env.NEXT_PUBLIC_USER_TYPE || '';
