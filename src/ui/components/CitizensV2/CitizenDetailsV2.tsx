6293c9c72714d7aec382526bfc46b836e1328559
ArturZzZZzZ <artur.zub@ideasoft.io>
2025-08-14 15:06:27 +0300

    feat: add person type

--- src/ui/components/CitizensV2/CitizenDetailsV2.tsx
+++ src/ui/components/CitizensV2/CitizenDetailsV2.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex, Image, Paper, Stack } from '@mantine/core';
+import { Flex, Paper, Stack } from '@mantine/core';
 import { AdditionalDetailsSection } from './components/AdditionalDetailsSection';
 import { PersonInfoSection } from './components/PersonInfoSection';
 import { PersonTypeSection } from './components/PersonTypeSection';
@@ -26,33 +26,6 @@ export interface CitizenProps {
   image: string | null;
 }
 
-const personTypeConfig = {
-  disability: {
-    title: 'Person with disability',
-    color: '#8B5CF6',
-    bgColor: '#F3F0FF',
-    icon: <Image src="/images/icons/disabledPerson.svg" alt="Disability Icon" />,
-  },
-  student: {
-    title: 'Student',
-    color: '#DC2626',
-    bgColor: '#FEF2F2',
-    icon: <Image src="/images/icons/student.svg" alt="Student Icon" />,
-  },
-  pensioner: {
-    title: 'Pensioner',
-    color: '#059669',
-    bgColor: '#F0FDF4',
-    icon: <Image src="/images/icons/pensioner.svg" alt="Pensioner Icon" />,
-  },
-  veteran: {
-    title: 'Veteran',
-    color: '#2563EB',
-    bgColor: '#EFF6FF',
-    icon: <Image src="/images/icons/veteran.svg" alt="Veteran Icon" />,
-  },
-};
-
 export const CitizenDetailsV2 = ({ citizen }: { citizen: CitizenProps }) => {
   const getPersonType = () => {
     if (citizen.hasDisabilities) {
@@ -61,11 +34,10 @@ export const CitizenDetailsV2 = ({ citizen }: { citizen: CitizenProps }) => {
     if (citizen.enrolledAsStudent) {
       return 'student';
     }
-    return 'disability';
+    return 'regular';
   };
 
   const personType = getPersonType();
-  const config = personTypeConfig[personType];
 
   const adaptedData = {
     socialNumber: citizen.socialNumber,
@@ -79,6 +51,9 @@ export const CitizenDetailsV2 = ({ citizen }: { citizen: CitizenProps }) => {
     personalNumber: citizen.pinfl,
     birthPlace: citizen.placeOfBirth,
     issuePlace: citizen.dateOfIssue,
+    disabilityGroup: citizen.disabilityGroup,
+    disabilityReason: citizen.disabilityReason,
+    universityName: citizen.universityName,
   };
 
   return (
@@ -86,7 +61,7 @@ export const CitizenDetailsV2 = ({ citizen }: { citizen: CitizenProps }) => {
       <Flex align="center" justify="center">
         <Stack gap="md" w="450px">
           <SocialNumberSection socialNumber={adaptedData.socialNumber} />
-          <PersonTypeSection data={adaptedData} config={config} />
+          <PersonTypeSection data={adaptedData} />
           <PersonInfoSection data={adaptedData} />
           <AdditionalDetailsSection data={adaptedData} />
         </Stack>
